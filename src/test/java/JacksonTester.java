b7c888d7b701b758e9a6525bae7a61d14041ceb8
legend3 <zhenguo50@126.com>
2021-01-08 01:13:17 +0800

    jackson教程

---
+++ src/test/java/JacksonTester.java
@@ -0,0 +1,305 @@
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 官网:http://jackson.codehaus.org/
+ * tutorial:http://jackson.codehaus.org/
+ *
+ * jackson库于2012.10.8号公布了最新的2.1版。因为有不少变更，这里做一个记录。
+ *
+ * jackson源码今朝托管于github，地址：https://github.com/fasterxml/
+ *
+ * 一、jackson 2.x
+ * jackson 2.x版供给了三个jar包供：
+ *
+ * 1．core库：streaming parser/generator，即流式的解析器和生成器。
+ * http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.1.0/jackson-core-2.1.0.jar
+ * 2．annotations库：databinding annotations，即带注释的数据绑定包。
+ * http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.1.0/jackson-annotations-2.1.0.jar
+ * 3．databind库：objectmapper， json tree model，即对象映射器，json树范型。
+ * http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.1.0/jackson-databind-2.1.0.jar
+ *
+ * 从jackson 2.0起，核心组件包含：jackson-annotations、jackson-core、jackson-databind。数据格局模块包含：smile、csv、xml、yam http://mvnrepository.com/artifact/com.fasterxml.jackson.core
+ *
+ * 二、jackson 1.9.10
+ * ackson 1.9.10版的库有多种选择：
+ *
+ * 单个库：供给了core-asl、mapper-asl、core-lpgl、mapper-lgpl、jax-rs、jax-xc、mrbean、smile等jar包。
+ * jackson all库：包含了上方所有的jar包，打包成了单个jar文件。
+ * http://jackson.codehaus.org/1.9.10/jackson-all-1.9.10.jar
+ * jackson mini库：包含了jackson-core库，打消了注释库、容许证文件、用于应用受限的景象，比如移动设备，jar包的尺寸明显削减。
+ * http://jackson.codehaus.org/1.9.10/jackson-mini-1.9.10.jar
+ * smile tool对象：一个号令行对象，用于在smile格局和json格局之间彼此转换。
+ * http://jackson.codehaus.org/1.9.10/smile-tool-1.9.10.jar
+ * 注：smile是二进制的json数据格局，等同于标准的json数据格局。smile格局于2010年公布，于2010年9月jackson 1.6版开端支撑。
+ * 支撑smile格局的框架：
+ * （1） jackson json processor：完全支撑smile格局，包含流式接见，数据绑定和树范型。
+ * （2） libsmile：一个c说话库，支撑读写smile数据。
+ * （3） elastic search：支撑把smile格局作为输入/输出的源。
+ * （4） protostuff：此项目支撑smile格局作为底层数据格局，也用于rpc的格局之一。
+ *
+ * 作者：一天一夜00
+ * 链接：https://www.jianshu.com/p/9ce3d110017e
+ * 来源：简书
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class JacksonTester {
+    @Test
+    public void test01() {
+        /**
+         * 创建一个包含学生详细信息的JSON字符串，
+         * 并将其反序列化为student对象，
+         * 然后将其序列化为JSON字符串。
+         */
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
+        //map json to student
+        try{
+            Student student = mapper.readValue(jsonString, Student.class);
+            System.out.println(student);
+            jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(student);
+            System.out.println(jsonString);
+        }
+        catch (JsonParseException e) { e.printStackTrace();}
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace(); }
+    }
+    @Test
+    public void test02(){
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
+        //map json to student
+        try{
+            Student student = mapper.readValue(jsonString, Student.class);
+            System.out.println(student);
+            jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(student);
+            System.out.println(jsonString);
+        }
+        catch (JsonParseException e) { e.printStackTrace();}
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace(); }
+    }
+    @Test
+    public void test03() {
+        JacksonTester tester = new JacksonTester();
+        try {
+            Student student = new Student();
+            student.setAge(10);
+            student.setName("Mahesh");
+            tester.writeJSON(student);
+            Student student1 = tester.readJSON();
+            System.out.println(student1);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void test04() {
+        JacksonTester tester = new JacksonTester();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String,Object> studentDataMap = new HashMap<String,Object>();
+            int[] marks = {1,2,3};
+            Student student = new Student();
+            student.setAge(10);
+            student.setName("Mahesh");
+            // JAVA Object
+            studentDataMap.put("student", student);
+            // JAVA String
+            studentDataMap.put("name", "Mahesh Kumar");
+            // JAVA Boolean
+            studentDataMap.put("verified", Boolean.FALSE);
+            // Array
+            studentDataMap.put("marks", marks);
+            mapper.writeValue(new File("student.json"), studentDataMap);
+            //result student.json
+            //{
+            //   "student":{"name":"Mahesh","age":10},
+            //   "marks":[1,2,3],
+            //   "verified":false,
+            //   "name":"Mahesh Kumar"
+            //}
+            studentDataMap = mapper.readValue(new File("student.json"), Map.class);
+            System.out.println(studentDataMap.get("student"));
+            System.out.println(studentDataMap.get("name"));
+            System.out.println(studentDataMap.get("verified"));
+            System.out.println(studentDataMap.get("marks"));
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void test05() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonString = "{\"name\":\"Mahesh Kumar\",  \"age\":21,\"verified\":false,\"marks\": [100,90,85]}";
+            JsonNode rootNode = mapper.readTree(jsonString);
+            JsonNode nameNode = rootNode.path("name");
+            System.out.println("Name: "+ nameNode.textValue());
+            JsonNode ageNode = rootNode.path("age");
+            System.out.println("Age: " + ageNode.intValue());
+            JsonNode verifiedNode = rootNode.path("verified");
+            System.out.println("Verified: " + (verifiedNode.booleanValue() ? "Yes":"No"));
+            JsonNode marksNode = rootNode.path("marks");
+            Iterator<JsonNode> iterator = marksNode.elements();
+            System.out.print("Marks: [ ");
+            while (iterator.hasNext()) {
+                JsonNode marks = iterator.next();
+                System.out.print(marks.intValue() + " ");
+            }
+            System.out.println("]");
+        }
+        catch (JsonParseException e) { e.printStackTrace(); }
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace();
+        }
+    }
+    @Test
+    public void test06() {
+        try {
+            JsonFactory jsonFactory = new JsonFactory();
+            JsonGenerator jsonGenerator = jsonFactory.createGenerator(new File("student.json"), JsonEncoding.UTF8);
+            jsonGenerator.writeStartObject();
+            // "name" : "Mahesh Kumar"
+            jsonGenerator.writeStringField("name", "Mahesh Kumar");
+            // "age" : 21
+            jsonGenerator.writeNumberField("age", 21);
+            // "verified" : false
+            jsonGenerator.writeBooleanField("verified", false);
+            // "marks" : [100, 90, 85]
+            jsonGenerator.writeFieldName("marks");
+            // [
+            jsonGenerator.writeStartArray();
+            // 100, 90, 85
+            jsonGenerator.writeNumber(100);
+            jsonGenerator.writeNumber(90);
+            jsonGenerator.writeNumber(85);
+            // ]
+            jsonGenerator.writeEndArray();
+            jsonGenerator.writeEndObject();
+            jsonGenerator.close();
+            //result student.json
+            //{
+            //   "name":"Mahesh Kumar",
+            //   "age":21,
+            //   "verified":false,
+            //   "marks":[100,90,85]
+            //}
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String,Object> dataMap = mapper.readValue(new File("student.json"), Map.class);
+            System.out.println(dataMap.get("name"));
+            System.out.println(dataMap.get("age"));
+            System.out.println(dataMap.get("verified"));
+            System.out.println(dataMap.get("marks"));
+        }
+        catch (JsonParseException e) { e.printStackTrace(); }
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace(); }
+    }
+    @Test
+    public void test066() {
+        JacksonTester tester = new JacksonTester();
+        try {
+            JsonFactory jsonFactory = new JsonFactory();
+            JsonGenerator jsonGenerator = jsonFactory.createGenerator(new File(
+                    "student.json"), JsonEncoding.UTF8);
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeStringField("name", "Mahesh Kumar");
+            jsonGenerator.writeNumberField("age", 21);
+            jsonGenerator.writeBooleanField("verified", false);
+            jsonGenerator.writeFieldName("marks");
+            jsonGenerator.writeStartArray(); // [
+            jsonGenerator.writeNumber(100);
+            jsonGenerator.writeNumber(90);
+            jsonGenerator.writeNumber(85);
+            jsonGenerator.writeEndArray();
+            jsonGenerator.writeEndObject();
+            jsonGenerator.close();
+            //result student.json
+            //{
+            //   "name":"Mahesh Kumar",
+            //   "age":21,
+            //   "verified":false,
+            //   "marks":[100,90,85]
+            //}
+            JsonParser jsonParser = jsonFactory.createParser(new File("student.json"));
+            while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+                //get the current token
+                String fieldname = jsonParser.getCurrentName();
+                if ("name".equals(fieldname)) {
+                    //move to next token
+                    jsonParser.nextToken();
+                    System.out.println(jsonParser.getText());
+                }
+                if("age".equals(fieldname)){
+                    //move to next token
+                    jsonParser.nextToken();
+                    System.out.println(jsonParser.getNumberValue());
+                }
+                if("verified".equals(fieldname)){
+                    //move to next token
+                    jsonParser.nextToken();
+                    System.out.println(jsonParser.getBooleanValue());
+                }
+                if("marks".equals(fieldname)){
+                    //move to [
+                    jsonParser.nextToken();
+                    // loop till token equal to "]"
+                    while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                        System.out.println(jsonParser.getNumberValue());
+                    }
+                }
+            }
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void test07() {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
+        //map json to student
+        try{
+            Student student = mapper.readValue(jsonString, Student.class);
+            System.out.println(student);
+            jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(student);
+            System.out.println(jsonString);
+        }
+        catch (JsonParseException e) { e.printStackTrace();}
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace();
+        }
+    }
+    //辅助调用方法
+    private void writeJSON(Student student) throws JsonGenerationException, JsonMappingException, IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File("student.json"), student);
+    }
+    private Student readJSON() throws JsonParseException, JsonMappingException, IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        Student student = mapper.readValue(new File("student.json"), Student.class);
+        return student;
+    }
+}
+
